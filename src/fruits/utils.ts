524add5abf90c6cb6e04eedcc2830d518c55b45e
Niklas Mårdby <niklas.mardby@gmail.com>
2024-12-06 14:38:26 +0100

    Added more assignments to code

---
+++ src/fruits/utils.ts
@@ -0,0 +1,26 @@
+export const getFruit = (name) => {
+	// använd fruits-arrayen och returnera det fruit-objekt som matchar name
+	// finns det ingen sådan fruit så returnera undefined
+};
+
+export const getIcon = (name) => {
+	// använd fruits-arrayen och returnera ikonen som matchar frukten name
+	// finns det ingen sådan frukt så returnera undefined
+};
+
+export const getColor = (name) => {
+	// använd fruits-arrayen och returnera färgen som matchar frukten name
+	// finns det ingen sådan frukt så returnera undefined
+};
+
+// gör en till funktion som man ger ett fruktnamn och som returnerar en frukts kategori
+
+export const numberOfColoredFruits = (color) => {
+	// använd fruits-arrayen och returnera hur många frukter det finns av en given färg color
+	// finns det inga frukter av den färgen så svara 0
+};
+
+export const getFruitsByCategory = (category) => {
+	// använd fruits-arrayen
+	// ta emot en kategori och returnera en array av alla frukter som ingår i den kategorin
+};
